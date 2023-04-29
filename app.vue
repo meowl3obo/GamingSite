ab1f57c269baf6caa8c12e07bb1a5c4a4d6ff5e4
meowl3obo <dear91304526@gmail.com>
2023-04-29 17:50:18 +0800

    first push

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const runtimeConfig = useRuntimeConfig()
+console.log(runtimeConfig.public)
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@pinia/nuxt'],
+  runtimeConfig: {
+    public: {
+      version: "",
+      apiUrl: "",
+    }
+  }
+})
